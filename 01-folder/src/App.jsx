5c9a58f7d6aeac80087ab58d391fc829beda141f
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-07-07 21:41:07 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <div>
+        <h2>
+          Yatharth
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
