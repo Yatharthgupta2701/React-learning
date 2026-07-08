ef889d2cfdf0b50fd1442793bc6301bd79d00de2
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-07-08 19:43:51 +0530

    02-Components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+      <div className="card">
+            <h1>Yatharth Gupta</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, totam.</p>
+        </div>
+  )
+}
+
+export default card
